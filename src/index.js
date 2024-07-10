6f597c6b5c955428313269da4b78d80c0b57cef8
ABU <38125084+Distance10086@users.noreply.github.com>
2024-07-10 14:56:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.xiaodi8.top": dockerHub,
+  "quay.xiaodi8.top": "https://quay.io",
+  "gcr.xiaodi8.top": "https://gcr.io",
+  "k8s-gcr.xiaodi8.top": "https://k8s.gcr.io",
+  "k8s.xiaodi8.top": "https://registry.k8s.io",
+  "ghcr.xiaodi8.top": "https://ghcr.io",
+  "cloudsmith.xiaodi8.top": "https://docker.cloudsmith.io",
+  "ecr.xiaodi8.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.xiaodi8.top": dockerHub,
 };
 
 function routeByHosts(host) {
